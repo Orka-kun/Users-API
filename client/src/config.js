bbfa3acbae85b4af6a0fa5c131a325741e546bbe
Orka Dasgupta <149400295+Orka-kun@users.noreply.github.com>
2025-03-31 02:08:35 +0600

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://users-api-odh2.onrender.com' 
-  : 'http://localhost:5000';
+export const API_URL = import.meta.env.VITE_API_URL || 
+  (import.meta.env.MODE === 'production' 
+    ? 'https://users-api-odh2.onrender.com' 
+    : 'http://localhost:5000');
