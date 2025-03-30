2315916cdc0906d7c9e33eb8f8ef52cb8dca5e27
Orka Dasgupta <149400295+Orka-kun@users.noreply.github.com>
2025-03-30 22:32:30 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,9 +18,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
 
 // Test connection
